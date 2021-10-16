ff084b131f09832a3c30d149cd55b0637864e91b
cec <gules01@gmail.com>
2021-10-16 02:07:11 +0200

    PostgreSQL connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,25 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
 
 @Module({
-  imports: [UsersModule, ReportsModule],
+  imports: [
+    UsersModule,
+    ReportsModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'keycloak',
+      password: 'password',
+      database: 'cars',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as morgan from 'morgan';
+
+const PORT = 3000;
+const logger = new Logger('Main');
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, {
+    logger: ['log', 'debug'],
+    // logger: false,
+  });
+
+  app.use(morgan('tiny'));
+
+  await app.listen(PORT);
 }
-bootstrap();
+bootstrap().then(() => logger.debug(`Starting Application on PORT: ${PORT}`));
