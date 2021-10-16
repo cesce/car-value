3b1956b6bb0b40a4d54ac19559078dad3f23ec1e
cec <gules01@gmail.com>
2021-10-17 01:46:32 +0200

    User update
    New route for users
    Created DTO to validate body
    Modified main.ts to whitelist

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as morgan from 'morgan';
@@ -13,6 +13,11 @@ async function bootstrap() {
   });
 
   app.use(morgan('tiny'));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
 
   await app.listen(PORT);
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,12 @@
-import { Controller, Logger } from '@nestjs/common';
+import { Body, Controller, Logger, Post } from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
 
-@Controller('users')
+@Controller('auth')
 export class UsersController {
   private readonly logger: Logger = new Logger(UsersController.name);
+
+  @Post('/signup')
+  createUser(@Body() body: CreateUserDto) {
+    this.logger.log(body);
+  }
 }
